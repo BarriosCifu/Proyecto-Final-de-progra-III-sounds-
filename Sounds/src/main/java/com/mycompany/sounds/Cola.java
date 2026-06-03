5f3cc38aa00a055a738cb5a01c86fcefb11b1668
BarriosCifu <102176372+BarriosCifu@users.noreply.github.com>
2026-06-02 19:05:40 -0600

    Add files via upload

--- Sounds/src/main/java/com/mycompany/sounds/Cola.java
+++ Sounds/src/main/java/com/mycompany/sounds/Cola.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sounds;
 
 /**
@@ -9,5 +5,39 @@ package com.mycompany.sounds;
  * @author barri
  */
 public class Cola {
-    
+    private NodoLista frente;
+    private NodoLista fin;
+
+    public Cola() {
+        this.frente = null;
+        this.fin = null;
+    }
+
+    // Agregar una canción a la cola de reproducción (Enqueue)
+    public void encolar(Cancion cancion) {
+        NodoLista nuevo = new NodoLista(cancion);
+        
+        if (frente == null) {
+            frente = nuevo;
+            fin = nuevo;
+        } else {
+            fin.setSiguiente(nuevo);
+            fin = nuevo;
+        }
+    }
+
+    // Sacar la siguiente canción a reproducir (Dequeue)
+    public Cancion desencolar() {
+        if (frente == null) {
+            return null; // No hay canciones en espera
+        }
+        Cancion extraida = frente.getCancion();
+        frente = frente.getSiguiente();
+        
+        if (frente == null) {
+            fin = null; // Si sacamos la única canción, la cola queda totalmente vacía
+        }
+        
+        return extraida;
+    }
 }
--- Sounds/src/main/java/com/mycompany/sounds/Pila.java
+++ Sounds/src/main/java/com/mycompany/sounds/Pila.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.sounds;
-
 /**
  *
  * @author barri
  */
+package com.mycompany.sounds;
+
+
 public class Pila {
-    
+  private NodoLista cima;
+
+    public Pila() {
+        this.cima = null;
+    }
+
+    // Agregar una canción al historial (Push)
+    public void push(Cancion cancion) {
+        NodoLista nuevo = new NodoLista(cancion);
+        if (cima != null) {
+            nuevo.setSiguiente(cima);
+        }
+        cima = nuevo;
+    }
+
+    // Sacar la última canción del historial (Pop)
+    public Cancion pop() {
+        if (cima == null) {
+            return null; // El historial está vacío
+        }
+        Cancion extraida = cima.getCancion();
+        cima = cima.getSiguiente();
+        return extraida;
+    }
+
+    // Ver cuál fue la última canción sin sacarla del historial (Peek)
+    public Cancion peek() {
+        if (cima != null) {
+            return cima.getCancion();
+        }
+        return null;
+    }  
 }
